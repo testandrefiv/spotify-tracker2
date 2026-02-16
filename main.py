04e1970510d88dba9968ff2e7a3eb3b327daf137
testandrefiv <wes@777sound.com>
2026-02-16 20:38:57 +0500

    Update main.py

--- main.py
+++ main.py
@@ -37,6 +37,10 @@ SENDER_PASSWORD = os.getenv("SENDER_PASSWORD", "")
 RECIPIENT_EMAIL = "andre@sevenstudios.se"
 
 # Database Setup
+# Database Setup
+if DATABASE_URL.startswith("postgres://"):
+    DATABASE_URL = DATABASE_URL.replace("postgres://", "postgresql://", 1)
+
 engine = create_engine(
     DATABASE_URL, 
     connect_args={"check_same_thread": False} if "sqlite" in DATABASE_URL else {}
